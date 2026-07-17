25b745106ff5cb2c5cc7e704a6268f113bb1104c
Dylan Roberts <81657406+realdyl@users.noreply.github.com>
2026-07-16 22:09:44 -0500

    style(aeo): implement AEO/SEO crawler updates

--- app/robots.ts
+++ app/robots.ts
@@ -2,12 +2,18 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: ['/'],
-      disallow: ['/api/', '/_next/', '/static/'],
-    },
-    sitemap: 'https://blxckchat.jexxx.us/sitemap.xml',
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/_next/', '/static/'],
+      },
+      {
+        userAgent: ['GPTBot', 'ClaudeBot', 'Google-Extended', 'PerplexityBot'],
+        allow: '/',
+      },
+    ],
+    sitemap: ['https://blxckchat.jexxx.us/sitemap.xml', 'https://blxckchat.jexxx.us/llms.txt'],
     host: 'https://blxckchat.jexxx.us',
   };
 }
